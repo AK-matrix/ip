eeb80c6b08185e50a8093c30fd908e3269472294
AK-matrix <arnavkamath2802@gmail.com>
2026-01-29 00:26:03 +0800

    AK: Configure Gradle
    - Updated build.gradle:\n    - Set main class to ak.AK.\n    - Renamed output jar to ak.jar.\n    - Configured Java toolchain to\nstrictly use Java 17.

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,24 +1,19 @@
 @ECHO OFF
 
-REM create bin directory if it doesn't exist
-if not exist ..\bin mkdir ..\bin
-
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 if exist data rmdir /s /q data
 if exist ..\data\ak.txt del ..\data\ak.txt
 
-REM compile the code into the bin folder
-REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\ak\*.java ..\src\main\java\ak\command\*.java ..\src\main\java\ak\exception\*.java ..\src\main\java\ak\parser\*.java ..\src\main\java\ak\storage\*.java ..\src\main\java\ak\task\*.java ..\src\main\java\ak\ui\*.java
+REM build the code using Gradle
+call ..\gradlew shadowJar
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
-REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin ak.AK < input.txt > ACTUAL.TXT
+java -jar ..\build\libs\ak.jar < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
